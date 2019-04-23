36ab54afed13973e44244023649d05344a74e893
Harrison Mamin <hmamin2@dons.usfca.edu>
2019-04-22 23:06:45 -0700

    pandas ends

--- htools/__init__.py
+++ htools/__init__.py
@@ -1,4 +1,5 @@
 from email.mime.text import MIMEText
+import pandas as pd
 import smtplib
 import time
 
@@ -16,6 +17,22 @@ class LambdaDict(dict):
     def __missing__(self, key):
         self[key] = self.f(key)
         return self[key]
+
+
+def ends(df, n=5):
+    """Show both the head and tail of a pandas dataframe.
+
+    Sample usage:
+    pd.DataFrame.ends = ends
+    df.ends(3)
+    df.sort_values('x').ends()
+
+    :param df: pandas DataFrame
+    :param n: Number of rows to display in head and tail, respectively. The
+    total number of rows returned will therefore be 2*n.
+    :return: pandas DataFrame
+    """
+    return pd.concat([df.head(n), df.tail(n)], axis=0)
 
 
 def hdir(obj):
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
+
 setup(name='htools',
-	  version='0.1',
-	  description='Harrison\'s custom functions.',
-	  packages=['htools'],
-	  author='Harrison Mamin',
-	  zip_safe=False)
+      version='0.1',
+      description='Harrison\'s custom functions.',
+      packages=['htools'],
+      author='Harrison Mamin',
+      zip_safe=False)
