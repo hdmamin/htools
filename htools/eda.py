0f43355e817133ee3a620d5d5d4980dc2579ebf5
Harrison Mamin <hmamin2@dons.usfca.edu>
2019-07-06 15:28:02 -0700

    eda

--- htools/eda.py
+++ htools/eda.py
@@ -1,3 +1,4 @@
+import operator
 import pandas as pd
 
 
@@ -15,3 +16,38 @@ def ends(df, n=5):
     :return: pandas DataFrame
     """
     return pd.concat([df.head(n), df.tail(n)], axis=0)
+
+
+def filter_by_count(df, col, value, method):
+    """Filter a dataframe to return a subset of rows determined by their
+     value_counts(). For example, we can return rows with users who appear
+     at least 5 times in the dataframe, or with users who appear less than 10
+     times, or who appear greater than or equal to 100 times.
+     """
+    operation = {'=': operator.eq,
+                 '>': operator.gt,
+                 '<': operator.lt,
+                 '>=': operator.ge,
+                 '<=': operator.le
+                }
+    counts = df[col].value_counts().loc[lambda x: operation[method](x, value)]
+    return df[df[col].isin(counts.index)]
+
+
+def top_cats(df, col, cats=None, val=None):
+    """Filter a df to only include the most common categories.
+
+    Parameters
+    -----------
+    cats: int
+        Optional - # of categories to include (i.e. top 5 most common
+        categories).
+    val: int
+        Optional - Value count threshold to include (i.e. all categories that
+        occur at least 10 times).
+    """
+    if cats is not None:
+        top = df[col].value_counts(ascending=False).head(cats).index
+        return df[df[col].isin(top)]
+    if val is not None:
+        return df.groupby(col).filter(lambda x: len(x) >= val)
