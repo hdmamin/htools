d98b6fc2cbe6cf4a378e3f3228f2bb307598f658
Harrison Mamin <hmamin2@dons.usfca.edu>
2019-10-11 23:07:26 -0700

    jupyter magic

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
 
+with open('requirements.txt', 'r') as f:
+    requirements = [line.strip() for line in f]
+
 setup(name='htools',
-      version='0.1',
-      description='Harrison\'s custom functions.',
-      packages=find_packages(include=['htools']),
-      author='Harrison Mamin',
-      zip_safe=False)
+    version='0.1',
+    description='Harrison\'s custom functions.',
+    packages=find_packages(include=['htools']),
+    author='Harrison Mamin',
+    zip_safe=False,
+    install_requires=requirements)
