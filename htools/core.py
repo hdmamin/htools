f15297f4f4aefd29fb09912593825d5c45a8d448
Harrison Mamin <hmamin2@dons.usfca.edu>
2020-03-31 09:49:27 -0700

    updated version of fuzzydict

--- htools/core.py
+++ htools/core.py
@@ -66,6 +66,70 @@ class InvalidArgumentError(Exception):
 
 
 class FuzzyKeyDict(dict):
+    """Dictionary that will try to find similar keys if a key is missing and
+    return their corresponding values. This could be useful when working with
+    embeddings, where we could try mapping missing words to a combination of
+    existing words.
+
+    Examples
+    --------
+    d = FuzzyKeyDict(limit=3, verbose=True)
+    d['dog'] = 0
+    d['cat'] = 1
+    d['alley cat'] = 2
+    d['pig'] = 3
+    d['cow'] = 4
+    d['cowbell'] = 5
+    d['baby cow'] = 6
+
+    # Keys and similarity scores for the most similar keys.
+    >>> d.similar_keys('house cat')
+    [('alley cat', 56), ('cat', 50), ('cowbell', 25)]
+
+    # "house cat" not in dict so we get the values for the most similar keys.
+    >>> d['house cat']
+    [2, 1, 5]
+
+    # "cat" is in dict so output is an integer rather than a list.
+    >>> d['cat']
+    1
+    """
+
+    def __init__(self, limit=3):
+        """
+        Parameters
+        ----------
+        limit: int
+            Number of similar keys to find when trying to retrieve the value
+            for a missing key.
+        """
+        self.limit = limit
+
+    def __getitem__(self, key):
+        """
+        Returns
+        -------
+        any or list[any]: If key is present in dict, the corresponding value
+            is returned. If not, the n closest keys are identified and their
+            corresponding values are returned in a list (where n is defined
+            by the `limit` argument specified in the constructor). Values are
+            sorted in descending order by the neighboring keys' similarity to
+            the missing key in.
+        """
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            return [self[k] for k in self.similar_keys(key)]
+
+    def similar_keys(self, key, return_distances=False):
+        pairs = process.extract(key, self.keys(), limit=self.limit,
+                                scorer=fuzz.ratio)
+        if return_distances:
+            return pairs
+        return [p[0] for p in pairs]
+
+
+class FuzzyKeyDict(dict):
     """Dictionary that tries to find similar keys if a key is missing and
     returns their corresponding values. This could be useful as a word2index
     mapping when working with embeddings: we could try mapping missing words to
