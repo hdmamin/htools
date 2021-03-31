6c35a24e6a85beff4cee41a368e0d1798b3e345f
Harrison Mamin <hmamin@goguardian.com>
2021-03-30 18:24:22 -0700

    readme updater tentatively working in nb

--- htools/cli.py
+++ htools/cli.py
@@ -1,4 +1,8 @@
 import fire
+from pathlib import Path
+import re
+
+from htools.core import save, load
 
 
 def Display(lines, out):
@@ -19,3 +23,50 @@ def Display(lines, out):
 
 fire.core.Display = Display
 
+
+# Start of htools CLI below. Stuff above is to import when building new CLIs
+# in other projects.
+class ReadmeUpdater:
+
+    def __init__(self, dirs):
+        self.dirs = [Path(d) for d in dirs]
+        self.extensions = {'.py', '.ipynb'}
+        self.docstring_chars = ('"""', "'''")
+        self.docstring_regex = '^(\'\'\'|\"\"\")Summary(.|\n)*(\'\'\'|\"\"\")$'
+
+    def process_files(self):
+        for dir_ in self.dirs:
+            self._process_dir_files(dir_)
+
+    def _process_dir_files(self, dir_):
+        self.path2summary = {path: self.parse_file() for path in dir_.iterdir()
+                             if path.suffix in self.extensions}
+
+    def parse_file(self, path):
+        return getattr(self, f'_parse_{path.suffix}')(path)
+
+    def _parse_py(self, path):
+        with open(path, 'r') as f:
+            text = f.read()
+        match = re.search(self.docstring_regex, text, re.MULTILINE)
+        # for i, line in enumerate(text.splitlines[:5]):
+        #     if line.startswith():
+        #         break
+
+
+    def _parse_ipynb(self, path):
+        pass
+
+
+def _update_readme():
+    pass
+
+
+def update_readme():
+    pass
+
+
+if __name__ == '__main__':
+    fire.Fire({
+        'update_readme': update_readme
+    })
